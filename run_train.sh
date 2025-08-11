eef2c1a294815cc5defecb382aae507c4924b507
3outeille <ferdinand.mom@epita.fr>
2025-08-11 11:36:39 +0000

    add fsdp + ep config

--- run_train.sh
+++ run_train.sh
@@ -12,12 +12,22 @@ set -ex
 # LOG_RANK=0,1 NGPU=4 ./run_train.sh
 NGPU=${NGPU:-"8"}
 export LOG_RANK=${LOG_RANK:-0}
+
+# Option to switch between debug and train
+MODE=${MODE:-"debug"}  # Set MODE=debug or MODE=train
+
 CONFIG_FILE=${CONFIG_FILE:-"./torchtitan/models/llama3/train_configs/debug_model.toml"}
+
+if [ "$MODE" = "debug" ]; then
+    PYTHON_CMD="debugpy-run -m torch.distributed.run --"
+else
+    PYTHON_CMD="torchrun"
+fi
 
 TORCHFT_LIGHTHOUSE=${TORCHFT_LIGHTHOUSE:-"http://localhost:29510"}
 
 PYTORCH_CUDA_ALLOC_CONF="expandable_segments:True" \
 TORCHFT_LIGHTHOUSE=${TORCHFT_LIGHTHOUSE} \
-torchrun --nproc_per_node=${NGPU} --rdzv_backend c10d --rdzv_endpoint="localhost:0" \
+$PYTHON_CMD --nproc_per_node=${NGPU} --rdzv_backend c10d --rdzv_endpoint="localhost:0" \
 --local-ranks-filter ${LOG_RANK} --role rank --tee 3 \
 -m torchtitan.train --job.config_file ${CONFIG_FILE} "$@"
